aced43a4fa4ccd423491253b6473b02fcfa0c886
Tieferson Domingos <tiefersond@yahoo.com.br>
2023-05-05 14:40:26 -0300

    Added new product

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('/', [IndexController::class,'index']);
 Route::post('qr-code', function (Request $request) {
     $url = "http://pqr.io/01/".str_pad($request->produto,14,"0",STR_PAD_LEFT)."/10/".$request->codigo."?17=".substr(preg_replace("/[^0-9]/","",$request->data),2,6);
     //return QrCode::size(200)->generate('www.google.com.br');
-   return QrCode::size(200)->format('png')->generate('www.google.com.br');
-    //return "<p><a target='_blank' href='{$url}'>{$url}</a></p><hr />"."<img src='data:image/png;base64, " .  base64_encode(QrCode::size(200)->format('png')->generate($url)). "'/><hr />";
+   //return QrCode::size(200)->format('png')->generate('www.google.com.br');
+    return "<p><a target='_blank' href='{$url}'>{$url}</a></p><hr />"."<img src='data:image/png;base64, " .  base64_encode(QrCode::size(200)->format('png')->generate($url)). "'/><hr />";
 });
